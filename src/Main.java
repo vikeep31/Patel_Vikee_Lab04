9d1ba11b8109b02b7aef755d445a95e5b83bc4ea
vikeep31 <113868550+vikeep31@users.noreply.github.com>
2022-09-18 21:43:16 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        double price=100;
+        //double price=in.nextDouble();
+        System.out.println("Price - $"+price+"\nSales Tax - $"+(price*0.05));
+    }
+}
+
